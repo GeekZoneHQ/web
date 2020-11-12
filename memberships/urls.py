99ffa04e6414aa7ae1179554910adbc425b592c0
James Geddes <j@jamesgeddes.pro>
2020-11-12 22:18:50 +0000

    remove commented code

--- memberships/urls.py
+++ memberships/urls.py
@@ -3,13 +3,10 @@ from . import views
 from django.contrib.auth.views import *
 
 urlpatterns = [
-    # path("/", views.home, name="memberships_home"),
     path("register/", views.register, name="register"),
     path("confirm/", views.confirm, name="confirm"),
     path("thanks/", views.thanks, name="thanks"),
     path("stripe-webhook/", views.stripe_webhook, name="stripe_webhook"),
-    # path("login/", views.login_view, name="memberships_login"),
-    # path("logout/", views.logout_view, name="memberships_logout"),
     path("settings/", views.settings_view, name="memberships_settings"),
     path("details/", views.details_view, name="memberships_details"),
     path('change-password/', PasswordChangeView.as_view()),
