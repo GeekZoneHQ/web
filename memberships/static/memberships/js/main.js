6bb1851f29a7ba1d9188ddf789277fa737da330f
tbentham <tristanbentham@yahoo.co.uk>
2020-12-09 18:30:01 +0000

    Implemented dark mode (plus minor changes).

--- memberships/static/memberships/js/main.js
+++ memberships/static/memberships/js/main.js
@@ -1,17 +1,33 @@
-let hello = () => console.log("hello");
+const hello = () => console.log("hello");
+var header;
 
-let header = document.getElementsByClassName("header-container");
-let sticky = header[0].offsetTop;
-
-window.onscroll = function() {toggleSticky()};
-toggleSticky();
-
-/*** Functions ***/
-
-function toggleSticky() {
-  if (window.pageYOffset > sticky) {
-    header[0].classList.add("sticky");
+// if scrolled down, make header sticky
+function checkSticky() {
+  if (!header)
+    header = document.getElementsByClassName("header-container")[0];
+  
+  if (window.pageYOffset > header.offsetTop) {
+    header.classList.add("sticky");
   } else {
-    header[0].classList.remove("sticky");
+    header.classList.remove("sticky");
   }
+}
+
+// check user's light/dark mode preference
+function checkDarkMode() {
+  if (localStorage.getItem("theme") == "dark") {
+    document.body.classList.add("dark-mode");
+  }
+}
+
+// change between light and dark modes
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+  
+  let theme = "light";
+  
+  if (document.body.classList.contains("dark-mode"))
+    theme = "dark";
+  
+  localStorage.setItem("theme", theme);
 }
