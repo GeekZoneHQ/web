77899c585119d98efea8d4bc92a7c1d519fadd8f
Carwyn Nelson <carwyn@carwynnelson.com>
2020-12-22 23:07:38 +0000

    blah

--- memberships/celery.py
+++ memberships/celery.py
@@ -3,6 +3,6 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web.settings')
 
-app = Celery('web')
+app = Celery('memberships')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
