ec43358c626db8238f6a2947ba6e3e97224de373
Carwyn Nelson <carwyn@carwynnelson.com>
2021-01-31 13:45:44 +0000

    Add the livereload server
    To make working on the front-end easier.

--- web/settings.py
+++ web/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "livereload",
     "django.contrib.staticfiles",
     "tailwind",
     "theme",
@@ -59,6 +60,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "livereload.middleware.LiveReloadScript",
 ]
 
 ROOT_URLCONF = "web.urls"
