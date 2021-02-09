5e3db94cc411f484732247157c0b5c19ce446097
James Geddes <j@jamesgeddes.pro>
2021-02-09 09:50:34 +0000

    outline required steps

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+echo "Welcome to the Geek.Zone/Web setup script!"
+echo "We know you don't have a choice when selecting setup scripts, so we are grateful that you chose this one"
+echo "Let's get readyyyyyy to prograaaaaam!\n\n"
+
+# Install Python3
+
+# Install pip3
+
+# Install nodejs
+
+apt install nodejs -y
+
+# install npm
+
+apt install npm -y
+
+# Install tailwind
+
+# Create venv
+
+# Activate venv
+
+# Install requirements
+
+# Migrate db
+
+# run django server
+
+# open the site in the default browser
