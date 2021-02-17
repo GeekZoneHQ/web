cdf0aac728e6807dd9cf1ae35b50cb1bb87e517a
James Geddes <j@jamesgeddes.pro>
2021-02-17 21:32:16 +0000

    correct sender and recipient name

--- memberships/email.py
+++ memberships/email.py
@@ -4,18 +4,18 @@ from django.conf import settings
 
 
 def send_email(to_name, to_email, subject, body):
-    # NB Must be handled via Celery
+    # NB Must be handled via Celery - tasks.py
     context = {
         'name': to_name,
         'email': to_email,
         'body': body
     }
-    email_body = render_to_string('email_message.html', context)
+    email_body = render_to_string('memberships/email_message.html', context)
 
     email = EmailMessage(
         subject,
         email_body,
         settings.DEFAULT_FROM_EMAIL,
-        [to_name],
+        [to_name + " <" + to_email + ">"],
     )
     return email.send(fail_silently=False)
