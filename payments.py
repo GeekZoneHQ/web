c62a9b3f52e6aa573ab06bf9348085d0c19eb895
SamWinterhalder <winterhalder.sam@gmail.com>
2021-02-19 16:59:05 +0000

    Fixed indentation error

--- payments.py
+++ payments.py
@@ -1,26 +1,26 @@
 from django.contrib.auth.models import Permission, User
 
-from .models import Member, Membership, FailedPayment, Payment
+from memberships.models import Member, Membership, FailedPayment, Payment
 from funky_time import epoch_to_datetime
 
 
 def handle_stripe_payment(event):
     if event["type"] == "invoice.payment_failed":
-        f_payment = FailedPayment.objects.create(
+        FailedPayment.objects.create(
             stripe_customer_id=event["data"]["object"]["customer"],
             stripe_subscription_id=event["data"]["object"]["subscription"],
             stripe_event_type=event["type"],
         )
-        if event["type"] == "invoice.payment_succeeded":
-            member = Member.objects.get(email=event["data"]["object"]["customer_email"])
-            log_payment(event, member)
-            update_last_payment(event, member)
-            add_user_sand_permission(member)
+    if event["type"] == "invoice.payment_succeeded":
+        member = Member.objects.get(email=event["data"]["object"]["customer_email"])
+        log_payment(event, member)
+        update_last_payment(event, member)
+        add_user_sand_permission(member)
 
 
 def log_payment(event, member):
     # Log payment for a member in the database
-    payment = Payment.objects.create(
+    Payment.objects.create(
         member=member,
         stripe_subscription_id=event["data"]["object"]["subscription"],
     )
