910dd736bd5060eeee880dea12d6773ed2e00876
tbentham <tbentham@users.noreply.github.com>
2021-04-02 16:27:03 +0100

    Stopped help text rendering offscreen.

--- memberships/static/memberships/js/main.js
+++ memberships/static/memberships/js/main.js
@@ -25,19 +25,45 @@ function toggleDarkMode() {
     localStorage.theme = "light";
 }
 
-function showHelpText(helpTextClassList) {
-  helpTextClassList.remove("opacity-0");
-  helpTextClassList.remove("animate-fade-out");
-  helpTextClassList.add("animate-fade-in");
-}
-
-function hideHelpText(helpTextClassList) {
-  helpTextClassList.add("opacity-0");
-  helpTextClassList.add("animate-fade-out");
-  helpTextClassList.remove("animate-fade-in");
-}
-
 // toggle visibility of header menu on smaller screens
 function toggleHeaderMenu() {
   document.getElementById("header-nav").classList.toggle("hidden");
+}
+
+// show popover help text
+function showHelpText(popover) {
+  let text = popover.firstElementChild;
+  let arrow = popover.lastElementChild;
+
+  correctOffscreenRight(text);
+
+  let popoverClassList = popover.classList;
+
+  popoverClassList.remove("opacity-0", "animate-fade-out");
+  popoverClassList.add("animate-fade-in");
+}
+
+// hide popover help text
+function hideHelpText(popover) {
+  let classList = popover.classList;
+
+  classList.add("opacity-0", "animate-fade-out");
+  classList.remove("animate-fade-in");
+}
+
+// check if an element is onscreen and translate if not
+function correctOffscreenRight(element) {
+  let rootFontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
+  let classList = element.classList;
+
+  // remove any existing translation
+  classList.remove(element.className.split(" ").find((a) => a.startsWith("-translate-x-")));
+
+  // get offscreen distance of element in rem units
+  let paddingRight = parseInt(window.getComputedStyle(element, null).getPropertyValue('padding-right'));
+  let offscreenRight = (element.getBoundingClientRect().right + paddingRight - window.screen.width) / rootFontSize + 0.5;
+  
+  // translate element if offscreen
+  if (offscreenRight > 0)
+    classList.add("-translate-x-" + (Math.floor(offscreenRight) * 4));
 }
