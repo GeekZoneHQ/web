cb9cb02df495d3d8bdc08f27af31a335138fe1e1
giulio-giunta <giuntalio@gmail.com>
2021-05-14 13:18:43 +0100

    this switches the database in settings.py from sqlite to postgres

--- web/settings.py
+++ web/settings.py
@@ -93,9 +93,8 @@ WSGI_APPLICATION = "web.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {"default": env.db(default="sqlite:/db.sqlite3")}
+#DATABASES = {"default": env.db(default="sqlite:/db.sqlite3")}
 
-'''
 # Use for production
 DATABASES = {
     "default": {
@@ -107,7 +106,6 @@ DATABASES = {
         "PORT": os.environ.get("DATABASE_PORT"),
     }
 }
-'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
