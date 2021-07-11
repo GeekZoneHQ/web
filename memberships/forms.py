c4c7d797db06f2ea662fc29845d54a8589d613b3
Freshrojek <jnboyer1@gmail.com>
2021-07-11 14:34:33 +0100

    Removed optional constitutional emal and post
    You can no longer uncheck constitutional email and post by adding it to `Exclude` in `forms.py` as suggested by @tbentham

--- memberships/forms.py
+++ memberships/forms.py
@@ -103,6 +103,8 @@ class MemberSettingsForm(ModelForm):
             "email",
             "user",
             "constitution_agreed",
+            "constitutional_email",
+            "constitutional_post",
             "renewal_date",
             "profile_image",
             "email_verified",
@@ -119,6 +121,8 @@ class MemberDetailsForm(ModelForm):
             "email",
             "user",
             "constitution_agreed",
+            "constitutional_email",
+            "constitutional_post",
             "profile_image",
-            "",
+
         ]
