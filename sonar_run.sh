9ca194913f17ff7c835acdcda6e2b6fd88d7befd
giulio-giunta <56529931+giulio-giunta@users.noreply.github.com>
2021-11-06 15:54:55 +0000

    This removes unnecessary lines in sonar_run.sh

--- sonar_run.sh
+++ sonar_run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
   
 docker-compose up -d 
+sleep 150
 sudo apt-get install jq -y 
 # PROJECTKEY="geekzone-sonar" 
 Check=`curl -s -u admin:geekzone http://localhost:9000/api/qualitygates/project_status?projectKey=geekzone-sonar | jq '.projectStatus.status'` 
