9af54d3191023f0e316ccc478c93db7784564dd9
giulio-giunta <56529931+giulio-giunta@users.noreply.github.com>
2021-11-06 13:46:31 +0000

    This adds configuration to run sonar in circleci

---
+++ sonar_run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash 
+docker-compose build  
+docker-compose up -d 
+sudo apt-get install jq -y 
+PROJECTKEY="geekzone-sonar" 
+Check=`curl -s -u admin:geekzone http://localhost:9000/api/qualitygates /project_status?projectKey=$PROJECTKEY | jq '.projectStatus.status' | tr - d '"'` 
+max_retry=30 
+counter=0 
+until [ "$Check" == "OK" ] || [ "$Check" = "ERROR" ] || [ "$Check" ==  "WARN" ]; 
+ do  
+ sleep 20 
+   
+ [[ counter -eq $max_retry ]] && echo "Failed! Check you sonnar-server  container" && exit 1 
+ echo "Retrying. Try #$counter" 
+ ((counter++))
+   
+Check=`curl -s -u admin:geekzone http://localhost:9000/api/qualitygates /project_status?projectKey=$PROJECTKEY | jq '.projectStatus.status' | tr - d '"'` 
+done 
+QGSTATUS=`curl -s -u admin:geekzone http://localhost:9000/api/qualitygates /project_status?projectKey=$PROJECTKEY | jq '.projectStatus.status' | tr - d '"'` 
+if [ "$QGSTATUS" = "OK" ] 
+then 
+echo "Status is OK" 
+echo "Stopping and removing docker images ...."  
+docker-compose down --rmi all 
+echo "Sonar scanning has successfully ended with the status 'OK'" 
+elif [ "$QGSTATUS" = "WARN" ] 
+then  
+echo "Status is WARN. Check out the quality of the products at  http://localhost:9000" 
+exit 1 # terminate and indicate error 
+elif [ "$QGSTATUS" = "ERROR" ] 
+then 
+echo "Status is ERROR. Check out the quality of the products at  http://localhost:9000" 
+exit 1 # terminate and indicate error 
+fi 
