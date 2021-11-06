4fc800e047d06d7e6beebd89304f83e63e955e6a
giulio-giunta <56529931+giulio-giunta@users.noreply.github.com>
2021-11-06 18:37:43 +0000

    This reverts to docker executor and fixes sonar_run.sh

--- sonar_run.sh
+++ sonar_run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
-  
+
 docker-compose up -d 
-sudo snap install jq
+# sudo snap install jq
 # sudo apt-get install jq -y 
 # PROJECTKEY="geekzone-sonar" 
 Check=`curl -s -u admin:geekzone http://localhost:9000/api/qualitygates/project_status?projectKey=geekzone-sonar | jq '.projectStatus.status'` 
 max_retry=30 
 counter=0 
-until [ "$Check" == '"OK"' ] || [ "$Check" = '"ERROR"' ] || [ "$Check" ==  '"WARN"' ]; 
+until [ "$Check" == '"OK"' ] || [ "$Check" == '"ERROR"' ] || [ "$Check" ==  '"WARN"' ]; 
  do  
  sleep 20 
    
@@ -18,17 +18,17 @@ until [ "$Check" == '"OK"' ] || [ "$Check" = '"ERROR"' ] || [ "$Check" ==  '"WAR
 Check=`curl -s -u admin:geekzone http://localhost:9000/api/qualitygates/project_status?projectKey=geekzone-sonar | jq '.projectStatus.status'` 
 done 
 QGSTATUS=`curl -s -u admin:geekzone http://localhost:9000/api/qualitygates/project_status?projectKey=geekzone-sonar | jq '.projectStatus.status'` 
-if [ "$QGSTATUS" = "OK" ] 
+if [ "$QGSTATUS" = '"OK"' ] 
 then 
 echo "Status is OK" 
 echo "Stopping and removing docker images ...."  
 docker-compose down --rmi all 
 echo "Sonar scanning has successfully ended with the status 'OK'" 
-elif [ "$QGSTATUS" = "WARN" ] 
+elif [ "$QGSTATUS" = '"WARN"' ] 
 then  
 echo "Status is WARN. Check out the quality of the products at  http://localhost:9000" 
 exit 1 # terminate and indicate error 
-elif [ "$QGSTATUS" = "ERROR" ] 
+elif [ "$QGSTATUS" = '"ERROR"' ] 
 then 
 echo "Status is ERROR. Check out the quality of the products at  http://localhost:9000" 
 exit 1 # terminate and indicate error 
