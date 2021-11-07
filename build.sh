a1bb3639d94144d9f4c7dca8634220079af81cba
giulio-giunta <56529931+giulio-giunta@users.noreply.github.com>
2021-11-07 20:01:36 +0000

    This modifies build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ docker push geekzone/tailwindcss
 TAILWIND=/usr/src/app/theme
 
 docker run -v theme:$TAILWIND -ti geekzone/tailwindcss \
-    yarn tailwind build $TAILWIND/static_src/src/styles.css -c $TAILWIND/static_src/tailwind.config.js -o $TAILWIND/static/css/dist/styles.css
+yarn tailwind build $TAILWIND/static_src/src/styles.css -c $TAILWIND/static_src/tailwind.config.js -o $TAILWIND/static/css/dist/styles.css
 
