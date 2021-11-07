a16b33609a786f836a79b4d4f0fe0a9dfb8211d4
giulio-giunta <56529931+giulio-giunta@users.noreply.github.com>
2021-11-07 15:41:24 +0000

    This adds ta Dockerfile and a script to run tailwind in a docker container

---
+++ build.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+docker build -t geekzone/tailwindcss -f docker/tailwindcss/Dockerfile .
+
+docker push geekzone/tailwindcss
+
+TAILWIND=/usr/src/app/theme
+
+docker run -v theme:$TAILWIND -ti geekzone/tailwindcss \
+    yarn tailwind build $TAILWIND/static_src/src/styles.css -c $TAILWIND/static_src/tailwind.config.js -o $TAILWIND/static/css/dist/styles.css
+
