0eadf708e13545cde8694dcb8864f765737f5240
giulio-giunta <56529931+giulio-giunta@users.noreply.github.com>
2021-11-14 09:46:34 +0000

    This adds tailwind in docker-compose

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #! /usr/bin/env bash
 docker build -t geekzone/tailwindcss -f docker/tailwindcss/Dockerfile .
 
-docker push geekzone/tailwindcss
+# docker push geekzone/tailwindcss
 
-TAILWIND=/usr/src/app/theme
-
-docker run -v theme:$TAILWIND -ti geekzone/tailwindcss \
-yarn tailwind build $TAILWIND/static_src/src/styles.css -c $TAILWIND/static_src/tailwind.config.js -o $TAILWIND/static/css/dist/styles.css
+# sudo docker run -v theme:$TAILWIND -ti geekzone/tailwindcss \
+docker run -ti geekzone/tailwindcss
+yarn tailwind build /usr/src/app/theme/static_src/src/styles.css -c /usr/src/app/theme/static_src/tailwind.config.js -o /usr/src/app/theme/static/css/dist/styles.css
 
