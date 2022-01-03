d2d7b5ca2ac4e77dd4c52cc3afa34d69b2b02699
giulio-giunta <56529931+giulio-giunta@users.noreply.github.com>
2022-01-03 01:35:44 +0000

    This removes extendretail/sonarcloud orb and substitutes it with scripts

---
+++ sonar/sonar_parameters.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -eo pipefail
+if [ "$CIRCLE_BRANCH" != "master" ]; then
+  # We need to fix the reference to parent, due to how CircleCI checkout code:
+  # https://discuss.circleci.com/t/git-checkout-of-a-branch-destroys-local-reference-to-master/23781
+  # https://community.sonarsource.com/t/code-is-empty-on-pull-request-reviews/822/11
+  git fetch --no-tags origin master
+  git branch -D master
+  git rev-parse origin/master
+fi
+
+sonarParams="-Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.login=$SONAR_AUTH_TOKEN \
+  -Dsonar.organization=$SONAR_ORGANIZATION \
+  -Dsonar.projectKey=${SONAR_ORGANIZATION}_${CIRCLE_PROJECT_REPONAME} \
+  -Dsonar.projectName=$CIRCLE_PROJECT_REPONAME"
+
+if [ -n "$CIRCLE_PULL_REQUEST" ]; then
+  pullRequestId=$(echo "$CIRCLE_PULL_REQUEST" | sed s/\\//\\n/g | tail -n 1)
+  sonarParams="$sonarParams \
+    -Dsonar.pullrequest.base=master \
+    -Dsonar.pullrequest.branch=$CIRCLE_BRANCH \
+    -Dsonar.pullrequest.key=$pullRequestId \
+    -Dsonar.pullrequest.provider=github \
+    -Dsonar.pullrequest.github.repository=$SONAR_ORGANIZATION/$CIRCLE_PROJECT_REPONAME"
+elif [ "$CIRCLE_BRANCH" != "master" ]; then
+  sonarParams="$sonarParams \
+    -Dsonar.branch.name=$CIRCLE_BRANCH"
+fi
+mkdir .scannerwork
+echo "$sonarParams" > .scannerwork/scanner_params.txt
---
+++ sonar/sonar_quality_gates.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -eo pipefail
+REPORT_PATH=".scannerwork/report-task.txt"
+CE_TASK_ID_KEY="ceTaskId="
+
+SONAR_INSTANCE="https://sonarcloud.io"
+SLEEP_TIME=5
+
+# get the compute engine task id
+ce_task_id=$(cat $REPORT_PATH | grep $CE_TASK_ID_KEY | cut -d'=' -f2)
+echo "Using task id of ${ce_task_id}"
+
+if [ -z "$ce_task_id" ]; then
+   echo "No task id found"
+   exit 1
+fi
+
+# grab the status of the task
+# if CANCELLED or FAILED, fail the Build
+# if SUCCESS, stop waiting and grab the analysisId
+wait_for_success=true
+
+while [ "${wait_for_success}" = "true" ]
+do
+  ce_status=$(curl -s -u "${SONAR_AUTH_TOKEN}": "${SONAR_INSTANCE}"/api/ce/task?id=${ce_task_id} | jq -r .task.status)
+
+  echo "Status of SonarQube task is ${ce_status}"
+
+  if [ "${ce_status}" = "CANCELLED" ]; then
+    echo "SonarQube Compute job has been cancelled - exiting with error"
+    exit 504
+  fi
+
+  if [ "${ce_status}" = "FAILED" ]; then
+    echo "SonarQube Compute job has failed - exit with error"
+    exit 500
+  fi
+
+  if [ "${ce_status}" = "SUCCESS" ]; then
+    wait_for_success=false
+  fi
+
+  sleep "${SLEEP_TIME}"
+
+done
+
+ce_analysis_id=$(curl -s -u $SONAR_AUTH_TOKEN: $SONAR_INSTANCE/api/ce/task?id=$ce_task_id | jq -r .task.analysisId)
+echo "Using analysis id of ${ce_analysis_id}"
+
+# get the status of the quality gate for this analysisId
+qg_status=$(curl -s -u $SONAR_AUTH_TOKEN: $SONAR_INSTANCE/api/qualitygates/project_status?analysisId="${ce_analysis_id}" | jq -r .projectStatus.status)
+echo "Quality Gate status is ${qg_status}"
+
+if [ "${qg_status}" != "OK" ]; then
+  echo "Quality gate is not OK - exiting with error"
+  exit 1
+fi
