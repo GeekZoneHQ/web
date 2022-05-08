ec3f03bedbbf32429aee43d30222c849feacad54
Geek.Zone/Bot <geekzonebot@users.noreply.github.com>
2022-05-08 13:40:54 +0000

    fixup: Format Python code with Black

--- memberships/tests/test_registration_form.py
+++ memberships/tests/test_registration_form.py
@@ -193,8 +193,7 @@ class DonationConfirmPageTestCase(StripeTestCase):
     def test_total_without_donation_shows_correct_amount(self):
         response = self.client.get(reverse("confirm"))
 
-        self.assertContains(
-            response, "Your sand membership will cost £1.00 a year")
+        self.assertContains(response, "Your sand membership will cost £1.00 a year")
         self.assertContains(
             response,
             "This is made up of a £1 Sand membership charge with no donation",
