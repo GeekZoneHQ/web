63412b32480928422a8e2ecf4bc52cc1acb6235a
Geek.Zone/Bot <geekzonebot@users.noreply.github.com>
2023-08-01 19:32:59 +0000

    fixup: Format Python code with Black

--- memberships/ai_user_avatar/main.py
+++ memberships/ai_user_avatar/main.py
@@ -6,12 +6,12 @@ openai.api_key = api_key
 
 def avatar_gen():
     response = openai.Image.create(
-    prompt="a user avatar that is a depiction of an alien, facing forward, shoulder up, gender ambiguous, a random color, make the style of the avatar abstract, and make sure the image avatar fills the space of the image",
-    n=1,
-    size="1024x1024"
+        prompt="a user avatar that is a depiction of an alien, facing forward, shoulder up, gender ambiguous, a random color, make the style of the avatar abstract, and make sure the image avatar fills the space of the image",
+        n=1,
+        size="1024x1024",
     )
-    image_url = response['data'][0]['url']
-    
+    image_url = response["data"][0]["url"]
+
     print(image_url)
 
 
