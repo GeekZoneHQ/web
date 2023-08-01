05a61bb797805a944c84d95a9b21825e577bdce8
AliGunez <gunesali97@gmail.com>
2023-08-01 20:20:36 +0100

    issue #640 user avatar
    This code generates a unique user avatar using the OpenAI API. #640

---
+++ memberships/ai_user_avatar/main.py
@@ -0,0 +1,18 @@
+import openai
+from API_KEY import api_key
+
+openai.api_key = api_key
+
+
+def avatar_gen():
+    response = openai.Image.create(
+    prompt="a user avatar that is a depiction of an alien, facing forward, shoulder up, gender ambiguous, a random color, make the style of the avatar abstract, and make sure the image avatar fills the space of the image",
+    n=1,
+    size="1024x1024"
+    )
+    image_url = response['data'][0]['url']
+    
+    print(image_url)
+
+
+avatar_gen()
