317e30ebe961b5f2c4085ef4b3683ea52d9fda06
AliGunez <gunesali97@gmail.com>
2023-10-18 02:19:45 +0100

    ai avatar generator component

---
+++ memberships/user_avatar_component_ai/API_KEY.py
@@ -0,0 +1,8 @@
+api_key = ""
+
+# Add openai api key
+# DO NOT MAKE API KEY PUBLIC
+""" This will incur charges (as of writing this the fee should be 1/5th of a penny per image), 
+It might be possible to get a non-profit grant from openai OR
+use new members donations to subsidize the avatar
+"""
